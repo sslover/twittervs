b97f96dc7cb3708588139d90fe70e8aef716c842
Sam Slover <samjslover@gmail.com>
2013-10-22 12:03:02 -0400

    commiting initial version of twittervs

---
+++ app.js
@@ -0,0 +1,245 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+  , io = require('socket.io')
+  , http = require('http')
+  , twitter = require('ntwitter')
+  , cronJob = require('cron').CronJob
+  , _ = require('underscore')
+  , path = require('path');
+
+// create a file system to write to the json file
+var fs = require('fs');
+var tweetJSON = [];
+
+//Create an express app
+var app = express();
+
+//Create the HTTP server with the express app as an argument
+var server = http.createServer(app);
+
+// Twitter topics array. We want a list for allTopics, and then we will check that list against the 2 topic lists
+var allTopics = [];
+var topic1 =  [];
+var topic2 = [];
+
+//These object structures keep track of the total number of tweets for each category, and a map of all the topics and how many tweets they've each gotten
+var topic1List = {
+    total: 0,
+    topics: {}
+};
+
+var topic2List = {
+    total: 0,
+    topics: {}
+};
+
+var currentTweet;
+var currentPhotoURL;
+var currentTweetURL;
+
+//Set the topic lists to zero.
+_.each(topic1, function(v) { topic1List.topics[v] = 0; });
+_.each(topic2, function(v) { topic2List.topics[v] = 0; });
+
+
+//Generic Express setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+// setup template engine - we're using Hogan-Express
+app.set('view engine', 'html');
+app.set('layout','layout');
+app.engine('html', require('hogan-express')); // https://github.com/vol4ok/hogan-express
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//We're using bower components so add it to the path to make things easier
+app.use('/components', express.static(path.join(__dirname, 'components')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+// the dataObject we are sending to the page, includes all the data we need
+var dataObject = {
+  topic1Master: topic1,
+  topic2Master: topic2,
+  topic1Data: topic1List, 
+  topic2Data: topic2List,
+  tweet: []
+};
+
+//Our only route! Render it with the current topic1List and topic2List
+app.get('/', function(req, res) {
+	res.render('index.html');
+});
+
+//Start a Socket.IO listen
+var sockets = io.listen(server);
+
+//Set the sockets.io configuration.
+//THIS IS NECESSARY ONLY FOR HEROKU!
+sockets.configure(function() {
+  sockets.set('transports', ['xhr-polling']);
+  sockets.set('polling duration', 10);
+});
+
+//If the client just connected, give them fresh data!
+sockets.sockets.on('connection', function(socket) { 
+
+  socket.emit('connection', dataObject);
+
+  //If someone enters new data, let's update it!
+  socket.on('newData', function (data) { 
+
+      console.log("new data!!! " + data.topic1 + " " + data.topic2);
+
+      //clear out the arrays
+      allTopics = [];
+      topic1 = [];
+      topic2 = [];
+
+      //now rebuild the arrays
+      allTopics.push(data.topic1);
+      allTopics.push(data.topic2);
+
+      topic1.push(data.topic1);
+      topic2.push(data.topic2);
+
+      dataObject.topic1Master = topic1;
+      dataObject.topic2Master = topic2;    
+
+      //now reset the total
+      topic1List.total = 0;
+      topic2List.total = 0;
+
+      //then clear out everything in the map
+      _.each(topic1, function(v) { topic1List.topics[v] = 0; });
+      _.each(topic2, function(v) { topic2List.topics[v] = 0; });
+
+      console.log("sending + $$$ + " + JSON.stringify(dataObject));
+      sockets.sockets.emit('newData', dataObject);
+
+      //Set up the twitter component
+      var t = new twitter({
+            consumer_key: 'DDFKrqAh5Gp0vT90rS1g1w', 
+            consumer_secret: 'Dk3E3NBQtxQsfdLPjUUU6dmLwMCeLlWIvpZkZojxIo',
+            access_token_key: '81014526-sEruflJpOTKZs8QDPqsjQ1OAYzvVGzCTpA6zsO3w',
+            access_token_secret: 'kHTOuiPhLSsKGlKXaZcZ3PjeudwlFZPjXfED0rpJ24'
+      });
+
+      //Tell the twitter API to filter on the allTopics, and then we will see if the topics are in our topic1 or topic2 list
+      // Here, we are filtering the location to just tweets in the United States
+      t.stream('statuses/filter', { track: allTopics}, function(stream) {
+
+        //We have a connection. Now watch the 'data' event for incomming tweets.
+        stream.on('data', function(tweet) {
+
+          //These variables are used to indicate whether a topic was actually mentioned.
+          var topic1Claimed = false;
+          var topic2Claimed = false;
+
+          //Make sure it was a valid tweet
+          if (tweet.text !== undefined) {
+
+            //We're gunna do some indexOf comparisons and we want it to be case agnostic.
+            var text = tweet.text.toLowerCase();
+
+            //Go through every topic1 and then topic2 and see if it was mentioned. If so, increment its counter and
+            //set its 'claimed' variable to true to indicate something was mentioned so we can increment
+            //the 'total' counter!
+            _.each(topic1, function(v) {
+                if (text.indexOf(v.toLowerCase()) !== -1) {
+                          topic1List.topics[v]++;
+                          topic1Claimed = true;
+                          dataObject.tweet.push(tweet.text);
+                          dataObject.tweet.push(tweet.user.profile_image_url);
+                          dataObject.tweet.push("https://twitter.com/" + tweet.user.screen_name +"/status/" + tweet.id_str);
+                          dataObject.tweet.push(v);
+                          dataObject.tweet.push(topic1List.topics[v]);
+                    }
+            });
+
+            _.each(topic2, function(v) {
+                if (text.indexOf(v.toLowerCase()) !== -1) {
+                          topic2List.topics[v]++;
+                          topic2Claimed = true;
+                          dataObject.tweet.push(tweet.text);
+                          dataObject.tweet.push(tweet.user.profile_image_url);
+                          dataObject.tweet.push("https://twitter.com/" + tweet.user.screen_name +"/status/" + tweet.id_str);
+                          dataObject.tweet.push(v);
+                          dataObject.tweet.push(topic2List.topics[v]);
+                    }
+
+            });
+
+            //If something was mentioned, increment the total counter and send the update to all the clients
+            if (topic1Claimed) {
+                //send tweet to master json
+                //writeFile(tweet);
+                //Increment total
+                topic1List.total++;
+                //Send to all the clients
+                sockets.sockets.emit('data', dataObject);
+                dataObject.tweet.length = 0; 
+            }
+
+            if (topic2Claimed) {
+                //send tweet to master json
+                //writeFile(tweet);
+                //Increment total
+                topic2List.total++;
+                //Send to all the clients
+                sockets.sockets.emit('data', dataObject);
+                dataObject.tweet.length = 0; 
+
+            }
+          }
+        });
+      });
+  });
+
+});
+
+
+
+function writeFile(tweet){
+
+  var newJSON = JSON.stringify(tweet);
+  tweetJSON.push(newJSON);
+  console.log(tweetJSON.length);
+  fs.writeFile('tweets.json', tweetJSON, function(err) {
+      if(err) {
+          console.log(err);
+      } else {
+          console.log("The file was saved!");
+      }
+  }); 
+
+}
+
+//Reset everything on a new day!
+new cronJob('0 0 0 * * *', function(){
+    //Reset the total
+    topic1List.total = 0;
+    topic2List.total = 0;
+
+    //Clear out everything in the map
+    _.each(topic1, function(v) { topic1List.topics[v] = 0; });
+    _.each(topic2, function(v) { topic2List.topics[v] = 0; });
+
+    //Send the update to the clients
+    sockets.sockets.emit('data', dataObject);
+
+}, null, true);
+
+//Create the server
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ public/js/script.js
@@ -0,0 +1,205 @@
+$(function() {
+    var socket = io.connect(window.location.hostname);
+
+    //need these to keep track of the starting count, so we can only show new data to the user
+    var startingTopic1Total;
+    var startingTopic2Total;
+    var startingTotal;
+
+    //tracks the lists of the the 2 topics
+    var topic1List;
+    var topic2List;
+
+    //tracks the total tweets for topic1, topic2, and total
+    var topic1Total;
+    var topic2Total;
+    var totalTweets;
+
+    //tracks the HTML tag of the topic name. Needed because some tags have spaces.
+    var topic1Tag;
+    var topic2Tag;
+
+    socket.on('connect', function() {
+        console.log("Connected");
+    });
+
+    // Receive a message
+    socket.on('connection', function(data) {
+
+        topic1List = data.topic1Master;
+        topic2List = data.topic2Master;
+
+        startingTopic1Total = data.topic1Data.total;
+        startingTopic2Total = data.topic2Data.total;
+        startingTotal = data.topic1Data.total + data.topic2Data.total;
+
+        //if there is an existing search, show it to the user!
+        if(data.topic1Master != ""){
+            $( "#go" ).show();
+            // let's file in the values of the forms with the 2 topics!
+            $('#t1').val(topic1List[0]);
+            $('#t2').val(topic2List[0]);
+
+            //the first time, loop through the lists and create the needed HTML
+            //first, we need to make sure the topic tag/name doesn't have any spaces
+            topic1Tag = topic1List[0].replace(/\s+/g, '-').toLowerCase();
+            topic2Tag = topic2List[0].replace(/\s+/g, '-').toLowerCase();
+
+            //now, let's create the HTML
+            for (var i = 0; i < topic1List.length; i++) {
+                $("#topic1").append('<div id="d1content">' +
+             '<h1 class="topic1">'+topic1List[0]+'</h1>' +
+             '<h4><span id="tweetCount-topic1">0</span> tweets</h4>' +
+             '<ul class="span5" id="'+topic1Tag+'">' +
+             '</ul>' +      
+             '<div style="clear:both"></div>' +
+             '</div>');
+            }
+
+            for (var i = 0; i < topic2List.length; i++) {
+                $("#topic2").append('<div id="d2content">' +
+             '<h1 class="topic2">'+topic2List[0]+'</h1>' +
+             '<h4><span id="tweetCount-topic2">0</span> tweets</h4>' +
+             '<ul class="span5" id="'+topic2Tag+'">' +
+             '</ul>' +      
+             '<div style="clear:both"></div>' +
+             '</div>');
+            }
+        }
+
+    });
+
+    // Receive a message
+    socket.on('newData', function(data) {
+
+        //get new topic(s)!
+        topic1List = data.topic1Master;
+        topic2List = data.topic2Master;
+
+        //reset starting total, so that when data comes in it starts off with the right number!
+        startingTopic1Total = data.topic1Data.total;
+        startingTopic2Total = data.topic2Data.total;
+        startingTotal = data.topic1Data.total + data.topic2Data.total;
+
+        $( "#go" ).show();
+        
+        //reset the old topics!
+        $( "#d1content" ).remove();
+        $( "#d2content" ).remove();
+
+        // let's file in the values of the forms with the 2 topics!
+        $('#t1').val(topic1List[0]);
+        $('#t2').val(topic2List[0]);
+
+        // for the top area of the page, here we do all the calculations on the total tweets and the percentage breakdown
+        $("#tweetCount").text("0");
+
+        var topic1Width = 50;
+        var topic2Width = 50;     
+
+        $("#topic1-chart").css("width", topic1Width +"%");
+        $("#topic2-chart").css("width", topic2Width +"%");
+
+        $("#topic1Data").text("waiting for data..");
+        $("#topic2Data").text("waiting for data..");
+
+
+        topic1List = data.topic1Master;
+        topic2List = data.topic2Master;
+
+        //the first time, loop through the lists and create the needed HTML
+        //first, we need to make sure the topic tag/name doesn't have any spaces
+        topic1Tag = topic1List[0].replace(/\s+/g, '-').toLowerCase();
+        topic2Tag = topic2List[0].replace(/\s+/g, '-').toLowerCase();
+
+        //the first time, loop through the lists and create the needed HTML
+        for (var i = 0; i < topic1List.length; i++) {
+            $("#topic1").append('<div id="d1content">' +
+         '<h1 class="topic1">'+topic1List[0]+'</h1>' +
+         '<h4><span id="tweetCount-topic1">0</span> tweets</h4>' +
+         '<ul class="span5" id="'+topic1Tag+'">' +
+         '</ul>' +      
+         '<div style="clear:both"></div>' +
+         '</div>');
+        }
+
+        for (var i = 0; i < topic2List.length; i++) {
+            $("#topic2").append('<div id="d2content">' +
+         '<h1 class="topic2">'+topic2List[0]+'</h1>' +
+         '<h4><span id="tweetCount-topic2">0</span> tweets</h4>' +
+         '<ul class="span5" id="'+topic2Tag+'">' +
+         '</ul>' +      
+         '<div style="clear:both"></div>' +
+         '</div>');
+        }
+    });
+
+
+    socket.on('data', function(data) {
+
+        topic1Total = data.topic1Data.total - startingTopic1Total;
+        topic2Total = data.topic2Data.total - startingTopic2Total;
+        totalTweets = (data.topic1Data.total + data.topic2Data.total) - startingTotal;
+
+        var tweet = data.tweet[0];
+        var photoURL = data.tweet[1]; 
+        var tweetURL = data.tweet[2]; 
+        var topic = data.tweet[3]; 
+        var topicCount = data.tweet[4]; 
+
+    // we are going to append the current tweet to the correct place
+    // first set up the html
+    var html = '<li><a href='+tweetURL+' target=\'_blank\'><img src='+photoURL+' height=\'30\' width=\'30\'></li>';
+
+    //first, we need to make sure the topic tag/name doesn't have any spaces
+    topic = topic.replace(/\s+/g, '-').toLowerCase();
+
+    //then append that to the correct topic
+    $("#"+topic).append(html);
+
+    // now let's update the number of tweets for that topic            
+    $('#tweetCount-topic1').text(topic1Total);
+    $('#tweetCount-topic2').text(topic2Total);
+
+    // for the top area of the page, here we do all the calculations on the total tweets and the percentage breakdown
+    $("#tweetCount").text(totalTweets);
+
+    var topic1Width = Math.round((topic1Total / totalTweets) * 100);
+    var topic2Width = Math.round((topic2Total / totalTweets) * 100);
+
+    if((topic1Width + topic2Width) > 100){
+        topic2Width = Math.floor((topic2Total / totalTweets) * 100);
+    }        
+
+    $("#topic1-chart").css("width", topic1Width +"%");
+    $("#topic2-chart").css("width", topic2Width +"%");
+
+    $("#topic1Data").text(topic1Width + "% (" + topic1Total +")");
+    $("#topic2Data").text(topic2Width + "% (" + topic2Total +")");
+
+
+
+    $('#last-update').text(new Date().toTimeString());
+
+    });
+
+// if they click to submit the new topics, get that info and send it to the server
+$( "#button1" ).click(function() {
+    var t1 = $( "#t1" ).val();
+    var t2 = $( "#t2" ).val();
+    console.log(t1 + " " + t2);
+    if (t1 == "" || t2 == ""){
+        alert( "Please do enter something for both topics! :)" );
+    }
+    else{
+        var data = {
+            topic1 : t1,
+            topic2 : t2
+        }
+        console.log("sending... " + data);
+        // Send it over sockets
+        socket.emit('newData', data);
+    }
+});
+
+})
